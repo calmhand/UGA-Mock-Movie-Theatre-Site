c483177653a248b51eab03d23c442c01ba9d8391
alyxndyr <johnny.goulb@gmail.com>
2022-09-04 15:17:56 -0400

    example stack

---
+++ example-stack/back/src/main/java/com/backend/example/ExampleApplication.java
@@ -0,0 +1,14 @@
+package com.backend.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+@SpringBootApplication
+public class ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApplication.class, args);
+	}
+
+}
