98d4b868c7fd658320821036f21a46fc9a5ad3ba
rysherk <83097096+rysherk@users.noreply.github.com>
2022-09-28 01:42:26 -0400

    Update UserOrders.vue
    Started making order summary page

--- front/src/views/Settings/UserOrders.vue
+++ front/src/views/Settings/UserOrders.vue
@@ -3,7 +3,16 @@
     <!-- Create a window with an example movie order. -->
     <!-- Showing ticket details and the price of each ticket, and the order total. The user should be able to
     update the order, delete a ticket or to confirm and continue to checkout.  -->
-    orders
+    <h1>Order Summary</h1><br>
+
+    Ticket Details:<br>
+    Ticket Prices:<br>
+    Order total:<br><br>
+
+    <button>Update Order</button>
+    <button>Delete Tickets</button>
+    <!-- Link to Checkout page -->
+    <button id="checkout-button">Checkout</button>
   </div>
 </template>
 
@@ -18,4 +27,9 @@ export default {
   #order-summaries-container {
     height: 100vh;
   }
+
+  #checkout-button {
+    animation: highlight 1s infinite; /* TODO: DELETE AFTER DEMO */
+  }
+
 </style>
