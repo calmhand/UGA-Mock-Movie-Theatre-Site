48694613a38629372f479bbd9711318214b46415
rysherk <83097096+rysherk@users.noreply.github.com>
2022-09-28 01:11:37 -0400

    Update ConfirmAccount.vue
    added confirmation code form placeholder

--- front/src/views/Login/ConfirmAccount.vue
+++ front/src/views/Login/ConfirmAccount.vue
@@ -1,6 +1,12 @@
 <template>
     <div id="confirm-form-container">
+
         <!-- Create a single input for w/ a placeholder to mimic a verification window. -->
+        <form id="account_confirmation_form">
+            <label>Confirmation Code</label>
+            <input type="text" placeholder="loremipsum"/><br>
+        </form>
+        
         <button @click="goToProfile()"> Verify Code </button>
     </div>
 </template>
@@ -20,4 +26,24 @@ export default {
     #confirm-form-container {
         height: 100vh;
     }
+    
+    form {
+        border: solid 1px white;
+        width: 400px;
+        margin: 10px auto;
+        padding: 40px;
+    }
+
+    input {
+        width: 250px;
+        height: 40px;
+        border: solid 4px white;
+        border-radius: 10px;
+        margin: 15px;
+    }
+
+    label {
+        font-size: 30px;
+    }
+
 </style>
