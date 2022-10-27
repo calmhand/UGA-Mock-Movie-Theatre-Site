bfb50de625e5360459036f325f8b034dae21c369
rysherk <83097096+rysherk@users.noreply.github.com>
2022-10-26 20:55:49 -0400

    updated EncryptionService.java
    added some methods to allow us to encrypt and decrypt data

--- backend/onlinemoviebooking/src/main/java/com/se/onlinemoviebooking/application/services/EncryptionService.java
+++ backend/onlinemoviebooking/src/main/java/com/se/onlinemoviebooking/application/services/EncryptionService.java
@@ -1,5 +1,47 @@
 package com.se.onlinemoviebooking.application.services;
 
-public class EncryptionService {
+import org.springframework.stereotype.Component;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.spec.SecretKeySpec;
+import javax.persistence.AttributeConverter;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.util.Base64;
+
+@Component
+public class EncryptionService implements AttributeConverter<String, String> {
+
+    private static final String ESS = "ESS";
+    private static final String SECRET = "secret-key-748297";
+
+    private final Key key;
+    private final Cipher cipher;
+
+    public EncryptionService() throws Exception {
+        key = new SecretKeySpec(SECRET.getBytes(), ESS);
+        cipher = Cipher.getInstance(ESS);
+    }
+    
+    @Override
+    public String convertToDatabaseColumn(String attribute) {
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(attribute.getBytes()));
+        } catch (IllegalBlockSizeException | BadPaddingException | InvalidKeyException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public String convertToEntityAttribute(String dbData) {
+        try {
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(dbData)));
+        } catch (InvalidKeyException | BadPaddingException | IllegalBlockSizeException e) {
+            throw new IllegalStateException(e);
+        }
+    }
 }
